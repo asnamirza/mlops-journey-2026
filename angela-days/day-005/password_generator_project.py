92a661e31c0a12d4cde17369de7201f61b60f83a
Asna Mirza <asnamirza9390@gmail.com>
2026-03-03 22:29:59 +0530

    Fix Day 5 exercises – corrected errors

--- angela-days/day-005/password_generator_project.py
+++ angela-days/day-005/password_generator_project.py
@@ -8,37 +8,17 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Easy Level
-# password = ""
-# for char in range(0, nr_letters):
-#     password += random.choice(letters)
-#
-# for char in range(0, nr_symbols):
-#     password += random.choice(symbols)
-#
-# for char in range(0, nr_numbers):
-#     password += random.choice(numbers)
-#
-# print(password)
-
-# Hard level
 password_list = []
+
+
 for char in range(0, nr_letters):
-    password_list.append(random.choice(letters))
-
+    password_list += random.choice(letters)
 for char in range(0, nr_symbols):
-    password_list.append(random.choice(symbols))
-
+    password_list += random.choice(symbols)
 for char in range(0, nr_numbers):
-    password_list.append(random.choice(numbers))
-
-print(password_list)
-random.shuffle(password_list)
-print(password_list)
-
+    password_list += random.choice(numbers)
 password = ""
 for char in password_list:
     password += char
 
-print(f"Your password is: {password}")
-
+print(f"Your password would be is: {password}")
