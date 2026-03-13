05cb6d6ad177aeda15466a8e547f2ab040d70525
Asna Mirza <asnamirza9390@gmail.com>
2026-03-13 23:18:58 +0530

    Complete Day 8 – Caesar Cipher & Love Calculator

---
+++ angela-days/day-008/functions_with_inputs.py
@@ -0,0 +1,6 @@
+def greet(name):
+    print(f"hello {name}")
+    print(f"how are you {name}")
+    print("hows your day going")
+
+greet("Asna")
---
+++ angela-days/day-008/positional_vs_keyword_arguments.py
@@ -0,0 +1,8 @@
+# Functions with input
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"How do you do like it in {location}?")
+
+
+greet_with(location="somewhere" , name="Asna")
